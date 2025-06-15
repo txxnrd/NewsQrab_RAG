4f5cda8c6467b8df06505480b708944c6b7fe6a7
txxnrd <nrbsld@korea.ac.kr>
2025-06-15 22:05:41 +0900

    feat:logger added

--- main.py
+++ main.py
@@ -294,8 +294,9 @@ async def generate_rag_script(req: RagRequest):
     original_script = req.originalScript
     characterA = req.characterA
     characterB = req.characterB
-    print("Character A:", req.characterA)
-    print("Character B:", req.characterB)
+    logger.info(f"Character A: {req.characterA}")
+    logger.info(f"Character B: {req.characterB}")
+
 
 
     # 캐릭터 스타일 가져오기
